57bb8e8cb5fa6f2ee14cc0bd69085282d13a2410
Tsaikkari <trospe@gmx.de>
2020-06-12 15:11:20 +0200

    Adding firebase, asynchronous redux action and tests

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -1,23 +1,31 @@
 import { v1 as uuid } from 'uuid';
+import database from '../firebase/firebase.js';
 
 // ADD_EXPENSE
-export const addExpense = (
-  {
-    description = '', // Default arguments...
-    note = '',
-    amount = 0,
-    createdAt = 0
-  } = {}
-) => ({
+export const addExpense = (expense) => ({
   type: 'ADD_EXPENSE',
-  expense: {          // ...used as properties
-    id: uuid(),
-    description,
-    note,
-    amount,
-    createdAt
-  }
+  expense
 });
+// Dispatches addExpense inside of the function (works because of thunk)
+// gets called with dispatch internally by redux 
+export const startAddExpense = (expenseData = {}) => {
+  return (dispatch) => {
+    const {
+      description = '', 
+      note = '',
+      amount = 0,
+      createdAt = 0
+    } = expenseData;
+    const expense = { description, note, amount, createdAt }
+    // Writing data to firebase and after that dispatch addExpense to change redux store
+    return database.ref('expenses').push(expense).then((ref) => {
+      dispatch(addExpense({
+        id: ref.key,
+        ...expense
+      }));
+    });
+  };
+};
 
 // REMOVE_EXPENSE
 export const removeExpense = ({ id } = {}) => ({
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,6 +1,9 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import expensesReducer from '../reducers/expenses';
 import filtersReducer from '../reducers/filters';
+import thunk from 'redux-thunk';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENTION_COMPOSE__ || compose;
 
 export default () => {
   const store = createStore(  // State and action passed in as arguments through reducers
@@ -8,7 +11,8 @@ export default () => {
       expenses: expensesReducer, // array on the prop handled by reducer
       filters: filtersReducer    // object on the prop handled by its reducer
     }), 
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+   // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+   composeEnhancers(applyMiddleware(thunk))
   );
   return store;
 };
