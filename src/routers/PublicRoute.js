7146cfd69fcbbf9302afbc9f2c05bb2785e79578
Tsaikkari <trospe@gmx.de>
2020-06-15 22:10:56 +0200

    Adding public routes

--- src/routers/PublicRoute.js
+++ src/routers/PublicRoute.js
@@ -1,30 +1,26 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Route, Redirect } from 'react-router-dom';
-import Header from '../components/Header';
 
-export const PrivateRoute = ({
+export const PublicRoute = ({
   isAuthenticated,
   component: Component,
   ...rest
 }) => (
     <Route {...rest} component={(props) => (
       isAuthenticated ? (
-        <div>
-          <Header />
-          <Component {...props} />
-        </div>
+        <Redirect to="/dashboard" />
       ) : (
-          <Redirect to="/" />
+        <Component {...props} />
         )
     )} />
-  );
-
+	);
+	
 const mapStateToProps = (state) => ({
-  isAuthenticated: !!state.auth.uid
+	isAuthenticated: !!state.auth.uid
 });
 
-export default connect(mapStateToProps)(PrivateRoute);
+export default connect(mapStateToProps)(PublicRoute);
 
 //Create PublicRoute 
 // Redirect to /dashboard if logged in
